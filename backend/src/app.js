cfd04c0738a4a7e0ba0cd43ac6b2ec6ba8d80c69
Cristian1-dev <cristianros211@gmail.com>
2025-08-24 17:32:51 -0600

    Quitando origin del backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ import cors from 'cors';
 
 export const app = express();
 app.use(cors({
-    origin: 'http://localhost:5173',
+    // origin: 'http://localhost:5173',
     credentials: true
 }));
 
